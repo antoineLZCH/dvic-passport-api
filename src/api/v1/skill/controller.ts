5fceec779b219260aa6fdaf7744b4c8315349be9
Axel André <98.axel.andre@gmail.com>
2020-04-30 17:04:32 +0200

    👌 IMPROVE: Add populated fields for the skills

--- src/api/v1/skill/controller.ts
+++ src/api/v1/skill/controller.ts
@@ -1,33 +1,38 @@
 import { Request, Response } from 'express'
 import SkillModel from './model'
 import ISkill from './interface'
+
 export default {
+
     async getAllSkills(req: Request, res: Response) {
         try {
-            const allSkills = await SkillModel.find({});
+            const allSkills = await SkillModel.find({}).populate('required_skills');
             return res.status(201).send(allSkills)
         } catch (e) {
             return res.sendStatus(500);
         }
     },
+
     async getSkill(req: Request, res: Response) {
         const { id } = req.params;
         try {
-            const newSkill = await SkillModel.findById(id);
+            const newSkill = await SkillModel.findById(id).populate('required_skills');
             return res.status(201).send(newSkill)
         } catch (e) {
             return res.sendStatus(500);
         }
     },
+
     async updateSkill(req: Request, res: Response) {
         const { id } = req.params;
         try {
-            const updated = await SkillModel.updateOne({_id: id}, req.body);
+            const updated = await SkillModel.updateOne({_id: id}, req.body).populate('required_skills');
             return res.status(200).send(updated)
         } catch (e) {
             return res.sendStatus(500);
         }
     },
+
     async deleteSkill(req: Request, res: Response) {
         const { id } = req.params;
         try {
@@ -37,6 +42,7 @@ export default {
             return res.sendStatus(500);
         }
     },
+
     async createSkill(req: Request, res: Response) {
         const newSkill: ISkill = req.body;
         try {
