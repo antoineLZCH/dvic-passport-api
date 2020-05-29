b5b0e84ea39c5ee47d47d6ff2b45c3061e1233f1
Antoine LZCH <antoine@agence-osez.com>
2020-05-29 15:05:26 +0200

    TRY removing dotenv in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import App from './app';
-import * as dotenv from 'dotenv';
-const env = dotenv.config();
 
-if (!env.error) {
-    const app = new App(process.env.PORT);
-    app.listen();
-} else {
-    throw env.error;
-}
+
+const app = new App(process.env.PORT);
+app.listen();
