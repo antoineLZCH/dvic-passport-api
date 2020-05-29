6173e7aa0a51e92bbff58da0adaaf0cdb2775fca
Antoine LZCH <antoine@agence-osez.com>
2020-05-29 13:10:54 +0200

    FIX IMPORTS

--- src/middlewares/isValidToken.ts
+++ src/middlewares/isValidToken.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import * as jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 
 import getTokenFromRequest from '../helpers/getTokenFromRequest'
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import App from './app';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 const env = dotenv.config();
 
-if(!env.error) {
+if (!env.error) {
     const app = new App(process.env.API_PORT);
     app.listen();
 } else {
