d20b1b0e1baf1ab18c8ca159d54da66a01238fb8
Antoine LZCH <antoine@agence-osez.com>
2020-05-29 13:25:23 +0200

    REMOVE PUBLIC from git

--- public/api/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paths = {
-    DEFAULT: '/',
-    V1: '/v1'
-};
-//# sourceMappingURL=constants.js.map
--- public/api/v1/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paths = {
-    DEFAULT: "/",
-    SKILL: "/skill",
-    USER: "/user"
-};
-//# sourceMappingURL=constants.js.map
