fb9ea3ee3ed89cdb00d4f3ccbd2c73c22d9edab0
Antoine LZCH <antoine@agence-osez.com>
2020-05-29 12:17:18 +0200

    FIX : Imports

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,7 +7,7 @@ const { DB_HOST, DB_PASSWORD, DB_NAME, DB_USER } = process.env;
 const SERVER_ADDRESS = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 // @ts-ignore
-mongoose.connect(SERVER_ADDRESS, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+mongoose.connect(SERVER_ADDRESS, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Database connected successfully');
 })
 
