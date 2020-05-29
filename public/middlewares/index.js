fb1cb7ce86e8a38b5cf6d9e84688fad09a8ba7c4
Antoine LZCH <antoine@agence-osez.com>
2020-05-29 11:25:40 +0200

    IGNORE PUBLIC

--- public/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var isValidToken_1 = require("./isValidToken");
-exports.isValidToken = isValidToken_1.default;
-//# sourceMappingURL=index.js.map
