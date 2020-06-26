e18afbf9dea6e8f2ac01de780c96e06e13829c0f
Antoine LZCH <antoinelozach.pro@gmail.com>
2020-06-26 17:07:08 +0200

    Fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,4 @@
 import App from './app';
-import dotenv from 'dotenv';
-const env = dotenv.config();
 
-if (!env.error) {
-    const app = new App(process.env.API_PORT);
-    app.listen();
-} else {
-    throw env.error;
-}
+const app = new App(process.env.API_PORT);
+app.listen();
