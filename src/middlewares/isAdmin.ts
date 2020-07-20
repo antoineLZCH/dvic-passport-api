00a947786d253102e56b7112c1abe6d80565d5bf
Axel André <98.axel.andre@gmail.com>
2020-07-20 14:44:09 +0200

    📦 NEW: Add isAdmin middleware

---
+++ src/middlewares/isAdmin.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express'
+
+interface IToken {
+    firstName: String,
+    lastName: String,
+    id: String,
+    email: String,
+    login_name: String,
+    infra_access: String,
+    iat: Number,
+}
+
+export default (req: Request, res: Response, next: NextFunction) => {
+    const user: IToken  = req.app.get('user');
+    const infraAccessList: String[] = user.infra_access.split(',');
+    if(infraAccessList.includes('admin'))
+        return next();
+    return res.sendStatus(403);
+}
